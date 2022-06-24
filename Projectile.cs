e496502fe3b5ac46de89af919202b94af6258fed
Gabriel Pombo <75410180+gabepombo1@users.noreply.github.com>
2022-06-24 11:39:11 -0800

    Update Projectile.cs

--- Projectile.cs
+++ Projectile.cs
@@ -10,26 +10,21 @@ public class Projectile : MonoBehaviour
 
     [SerializeField] public GameObject projectile;
     private float Speed = 2;
-    private float Damage;
+    private float Damage = 10;
 
     void Move()
     {
-        
-        //the projectile script will be attached tot he minion, so you can call it with gameObject
         Vector3 targetPosition = gameObject.GetComponent<Minion>().PickTarget(
             gameObject.GetComponent<VisionRange>().enemyTargets).transform.position;
         
         gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition, Speed);
-
     }
 
     private void OnCollisionEnter(Collision other)
-    {
-        
+    {        
         Destroy(gameObject);
         float newHealth = other.gameObject.GetComponent<Minion>().health - Damage;
         other.gameObject.GetComponent<Minion>().health = newHealth;
-
     }
 
     void Start()
