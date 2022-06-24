be5f5c6aecc4c70d419bb889350f3e668cdb3c71
Gabriel Pombo <75410180+gabepombo1@users.noreply.github.com>
2022-06-24 11:36:09 -0800

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using Vector3 = UnityEngine.Vector3;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] public GameObject projectile;
+    private float Speed = 2;
+    private float Damage;
+
+    void Move()
+    {
+        
+        //the projectile script will be attached tot he minion, so you can call it with gameObject
+        Vector3 targetPosition = gameObject.GetComponent<Minion>().PickTarget(
+            gameObject.GetComponent<VisionRange>().enemyTargets).transform.position;
+        
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition, Speed);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        Destroy(gameObject);
+        float newHealth = other.gameObject.GetComponent<Minion>().health - Damage;
+        other.gameObject.GetComponent<Minion>().health = newHealth;
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ VisionRange.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class VisionRange : MonoBehaviour
+{
+        
+        [SerializeField] private Collider radius;
+        [SerializeField] private string[] enemyTeamTags;
+        [SerializeField] public Queue<GameObject> enemyTargets;
+
+        //for when the unit spawns and there are enemies that will be in the radius without having collided with the unit's VisionRange
+        void InitEnemyTargets()
+        {
+
+                Minion unit = gameObject.GetComponent<Minion>();
+                string[] unitEnemyTeamArray = unit.enemyTeams;
+                int unitEnemyTeamListLength = unitEnemyTeamArray.Length;
+
+        }
+
+        //this will just keep enqueueing forever, find a way to limit the queue to 5 targets
+        void OnCollisionEnter(Collision other)
+        {
+
+                for (int i = 0; i < enemyTeamTags.Length; i++) {
+                        
+                        if (other.gameObject.CompareTag(enemyTeamTags[i]))
+                        {
+
+                                enemyTargets.Enqueue(other.gameObject);
+
+                        }
+
+                }
+
+                if (enemyTargets.Count > 5)
+                {
+
+                        enemyTargets.Dequeue();
+
+                }
+                
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+                enemyTeamTags = gameObject.GetComponent<Minion>().enemyTeams;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+                
+
+        }
+        
+}
